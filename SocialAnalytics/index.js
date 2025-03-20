927df0a5e85f7cc1c529dbbf9d6c554fe5fe113d
Ujjawal-Kantt <uk1287@srmist.edu.in>
2025-03-20 18:57:56 +0530

    Initiated the first project.

---
+++ SocialAnalytics/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./config/db');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+    next();
+});
+
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Database connection failed:', err);
+    } else {
+        console.log('Database connected successfully at:', res.rows[0].now);
+    }
+});
+
+// API routes
+app.get('/', (req, res) => {
+    res.send('Server is running, and the database connection is established!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
